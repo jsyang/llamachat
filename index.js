4e2fb25142eeeb4ccdeca3e4b1af94d00ac45439
jsyang <jsyang.ca@gmail.com>
2023-10-12 16:32:33 +0100

    Update with explanation of client msg storage

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use('*', async (c, next) => {
 app.get('/*', serveStatic({ root: './' }));
 
 app.post('/chat', async (c) => {
+	// The entire conversation is stored and sent via client rather than relying on a vector DB
+	// The model still has a max input token length of 768 regardless of where the input comes from!
 	const { messages } = await c.req.json();
 
 	const ai = new Ai(c.env.AI);

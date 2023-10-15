b607698fb2890debea1648d44a9e3e46b4a43e63
jsyang <jsyang.ca@gmail.com>
2023-10-15 23:37:23 +0100

    Add shannon entropy as a factor in truncation

--- helpers.js
+++ helpers.js
@@ -6,16 +6,56 @@ export function formatMessage(msg) {
     return `${msg.role}: ${msg.content}`;
 }
 
-// Search around the middle for system messages to drop when chopping
-const searchMiddleSequence = [0, 1, -1, 2, -2];
+// Returns a number between 1 - 9
+function getShannonEntropy(buf, start = 0, end) {
+    const H = 0;
+    end = end || buf.length;
+    const onebycount = 1 / (end - start);
+    const keys = [];
+    const C = {};
+    let val;
 
+    for (let i = start; i < end; i++) {
+        val = buf[i];
+        if (typeof C[val] == 'undefined') {
+            keys.push(val);
+            C[val] = onebycount;
+        } else {
+            C[val] += onebycount;
+        }
+    }
+
+    for (let i = 0; i < keys.length; i++) {
+        H += C[keys[i]] * Math.log(C[keys[i]]);
+    }
+
+    return -H / Math.LN2 + 1;
+}
+
+const WEIGHTS_ROLE = {
+    user: 1,
+    system: 4,  // Prefer to lose system messages over user ones
+};
+
+const getPruneWeight = m => WEIGHTS_ROLE[m.role] / getShannonEntropy(m.content);
+const sortByPruneWeightAscending = (a, b) => getPruneWeight(a) - getPruneWeight(b);
+
+
+// Select this many elements before and after the truncation position to be candidates for the next truncation
+const pruneRadius = 4;
+
+// Drop messages to fit inside the context window
+// Use a truncation location strategy and an entropy measure to figure out what to drop
 export function truncateMessages(messages, chopStrategy = 'middle') {
     switch (chopStrategy) {
         case 'middle':
             const index = messages.length >> 1;
-            // Prefer to lose generated messages rather than user input
-            const foundMessageToDrop = searchMiddleSequence.findIndex(offset => messages[offset + index].role === 'system');
-            messages.splice(foundMessageToDrop || index, 1);
+            const candidate = [
+                ...messages.slice(index - pruneRadius, index),
+                ...messages.slice(index, index + pruneRadius),
+            ].sort(sortByPruneWeightAscending).pop();
+
+            messages.splice(messages.findIndex(m => m === candidate), 1);
             break;
         case 'end':
             messages.pop();
@@ -39,3 +79,4 @@ export function chunkString(s, length) {
 
     return chunks;
 }
+
